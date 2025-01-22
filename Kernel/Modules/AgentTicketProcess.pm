4d20a8895f1c86538dfe0f470d921d44e50bded0
Denny Korsukéwitz <dennykorsukewitz@gmail.com>
2025-01-22 12:38:25 +0000

    Fixed Bug - Problem when handling errors in the owner field in the activity dialog [#627](https://github.com/znuny/Znuny/issues/627).

--- Kernel/Modules/AgentTicketProcess.pm
+++ Kernel/Modules/AgentTicketProcess.pm
@@ -3564,7 +3564,7 @@ sub _RenderOwner {
 
     # set server errors
     my $ServerError = '';
-    if ( IsHashRefWithData( $Param{Error} ) && $Param{Error}->{'Owner'} ) {
+    if ( IsHashRefWithData( $Param{Error} ) && $Param{Error}->{'OwnerID'} ) {
         $ServerError = 'ServerError';
     }
 
